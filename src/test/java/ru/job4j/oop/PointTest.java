68c34e571e9709513ec7ea9224576ee596786bb5
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-09-16 12:18:33 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,25 +1,61 @@
 package ru.job4j.oop;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.closeTo;
-import static org.junit.Assert.assertThat;
 
 public class PointTest {
 
     @Test
-    public void testDistance() {
+    public void when00to20then2() {
+        double expected = 2;
         Point a = new Point(0, 0);
-        Point b = new Point(4, 0);
-        double rsl = a.distance(b);
-        assertThat(rsl, closeTo(4, 0.001));
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void testDistance1() {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 5);
-        double rsl = a.distance(b);
-        assertThat(rsl, closeTo(5, 0.001));
+    public void when01to21then2() {
+        double expected = 2;
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 1);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when02to02then0() {
+        double expected = 0;
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 2);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when60to20then4() {
+        double expected = 4;
+        Point a = new Point(6, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when050to020then3() {
+        double expected = 3;
+        Point a = new Point(0, 5, 0);
+        Point b = new Point(0, 2, 0);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when418to621then7() {
+        double expected = 7.35;
+        Point a = new Point(4, 1, 8);
+        Point b = new Point(6, 2, 1);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
