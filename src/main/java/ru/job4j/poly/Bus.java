2bb856f6a55b40794e4d3f3d5efd7846e7f615b3
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-09-28 14:35:26 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transpor {
+    @Override
+    public void go() {
+    }
+
+    @Override
+    public void passengers(int numbers) {
+    }
+
+    @Override
+    public int refuelPrice(double liters) {
+        return 0;
+    }
+}
+
---
+++ src/main/java/ru/job4j/poly/Transpor.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transpor {
+     void go();
+
+     void passengers(int numbers);
+
+     int refuelPrice(double liters);
+}
