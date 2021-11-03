ed5e49904cb34e1aeeb42cbdc8af3a237c14a84c
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-11-03 21:35:24 +0300

    Телефонный справочник на базе ArrayList

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByNameFrom2() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Max", "Smirnov", "457899", "Spb")
+        );
+        ArrayList<Person> result = phones.find("Smir");
+        assertThat(result.get(0).getPhone(), is("457899"));
+    }
+}
