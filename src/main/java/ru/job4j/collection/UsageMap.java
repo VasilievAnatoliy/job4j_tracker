5bde68c3a7ee741c06071c9f32073f6dc651e938
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-11-15 13:50:36 +0300

    Map, HashMap

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("first@yandex.ru", "Ivanov Ivan Ivanovich");
+        map.put("second@yndex.ru", "Petrov Peter Petrovich");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
