76eba317e67533f57c102bbdfbe7f1fdd5806eb0
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-12-03 13:56:20 +0300

    Встроенные функциональные интерфейсы

--- src/test/java/ru/job4j/function/SearchFolderTest.java
+++ src/test/java/ru/job4j/function/SearchFolderTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SearchFolderTest {
+
     @Test
     public void whenFilterSize() {
         List<Folder> list = List.of(
