c4b95ca9d35b435b2520d747d934b1da42b6178e
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-12-03 16:32:36 +0300

    Лямбда

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -7,18 +7,9 @@ public class FI {
     public static void main(String[] args) {
 
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        /*
-        Arrays.sort(strings, cmpText);
-        System.out.println(Arrays.toString(strings));
 
-         */
-
-        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
-        /*
-        Arrays.sort(strings, cmpDescSize);
-        System.out.println(Arrays.toString(strings));
-
-         */
+        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(right.length(),
+                left.length());
     }
 
 }
