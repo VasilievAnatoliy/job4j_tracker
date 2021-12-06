bd640db009ba22d8fca832d35843e650ed4996aa
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-12-06 22:57:23 +0300

    Список адресов

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    @Test
+    public void whenAddAddress() {
+        List<Profile> list = List.of(
+                new Profile(new Address("Sochi", "1 Line", 33, 2)),
+                new Profile(new Address("Sochi", "3 Line", 2, 10)),
+                new Profile(new Address("Moscow", "Lenina", 24, 142))
+        );
+        List<Address> rsl = new Profiles().collect(list);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Sochi", "1 Line", 33, 2));
+        expected.add(new Address("Sochi", "3 Line", 2, 10));
+        expected.add(new Address("Moscow", "Lenina", 24, 142));
+        assertThat(rsl, is(expected));
+    }
+
+}
