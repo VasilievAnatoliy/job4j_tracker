86d3246d431b31490fea4fffcf6ba502c026162e
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-12-08 13:19:49 +0300

    Список адресов

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(s -> s.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 }
