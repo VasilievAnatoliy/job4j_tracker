b324a02d156f6c4ae3feb1634cc66d50b5938a9e
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-12-08 15:31:53 +0300

    Уникальность элементов и сортировка.

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -10,18 +10,36 @@ import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
     @Test
-    public void whenAddAddress() {
+    public void whenAddAddressAndSortCity() {
         List<Profile> list = List.of(
                 new Profile(new Address("Sochi", "1 Line", 33, 2)),
-                new Profile(new Address("Sochi", "3 Line", 2, 10)),
-                new Profile(new Address("Moscow", "Lenina", 24, 142))
+                new Profile(new Address("Moscow", "Lenina", 24, 142)),
+                new Profile(new Address("Sochi", "3 Line", 2, 10))
         );
         List<Address> rsl = new Profiles().collect(list);
         List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Lenina", 24, 142));
         expected.add(new Address("Sochi", "1 Line", 33, 2));
         expected.add(new Address("Sochi", "3 Line", 2, 10));
-        expected.add(new Address("Moscow", "Lenina", 24, 142));
         assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSortCityAndDuplicates() {
+        List<Profile> list = List.of(
+                new Profile(new Address("Sochi", "1 Line", 33, 2)),
+                new Profile(new Address("Moscow", "Lenina", 24, 142)),
+                new Profile(new Address("Moscow", "Lenina", 24, 142)),
+                new Profile(new Address("Sochi", "3 Line", 2, 10)),
+                new Profile(new Address("Sochi", "3 Line", 2, 10))
+        );
+        List<Address> rsl = new Profiles().collect(list);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Lenina", 24, 142));
+        expected.add(new Address("Sochi", "1 Line", 33, 2));
+        expected.add(new Address("Sochi", "3 Line", 2, 10));
+        assertThat(rsl, is(expected));
+
     }
 
 }
