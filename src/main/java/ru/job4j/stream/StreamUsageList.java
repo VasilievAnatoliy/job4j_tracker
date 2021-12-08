d7fb628377806a070813527c939cdf39af02fa77
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-12-08 13:12:46 +0300

    Stream API

--- src/main/java/ru/job4j/stream/StreamUsageList.java
+++ src/main/java/ru/job4j/stream/StreamUsageList.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 public class StreamUsageList {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(2, -5, 4, -1, 7));
-        List<Integer> positiveNumbers = list.stream().filter(
-                s -> s > 0).collect(Collectors.toList());
+        List<Integer> positiveNumbers = list.stream()
+                .filter(s -> s > 0)
+                .collect(Collectors.toList());
     }
 }
